a1eadbc33178da0a2117cad71a7cf6e4a3f16937
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-06-05 21:07:54 +0000

    rebaseee

---
+++ Application/Commands/Todo/Handlers/GetAllTodoRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using gym.Application.Commands.Todo.Requests;
+using gym.Application.DTOs.TodoDtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace gym.Application.Commands.Todo.Handlers
+{
+    public class GetAllTodoRequestHandler : IRequestHandler<GetAllTodoRequest, List<TodoDto>>
+    {
+        public Task<List<TodoDto>> Handle(GetAllTodoRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Commands/Todo/Requests/UpdateTodoComand.cs
@@ -0,0 +1,13 @@
+﻿using gym.Application.DTOs.TodoDtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gym.Application.Commands.Todo.Requests
+{
+    public class UpdateTodoComand : IRequest<Unit>
+    {
+        public UpdateTodoDto updateDto { get; set; }
+    }
+}
